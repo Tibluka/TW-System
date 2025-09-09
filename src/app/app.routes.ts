08390d7060ea26392a1fedf3d60b65c1c26aa26e
Lucas <P0659387@portoseguro.com.br>
2025-09-08 21:11:32 -0300

    Implement initial routing and structure for the application, including login and home components with authentication guard. Update app component template and styles for better organization.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/authorized/home/home.component';
+import { authenticationGuard } from './services/guards/authentication.guard';
+import { LoginComponent } from './pages/not-authorized/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        pathMatch: 'full',
+        redirectTo: 'login'
+    },
+    {
+        path: 'login',
+        component: LoginComponent
+    },
+    {
+        path: 'home',
+        component: HomeComponent,
+        canActivate: [authenticationGuard]
+    }
+];
---
+++ src/app/pages/authorized/production-orders/production-orders.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-production-orders',
+  imports: [],
+  templateUrl: './production-orders.component.html',
+  styleUrl: './production-orders.component.scss'
+})
+export class ProductionOrdersComponent {
+
+}
