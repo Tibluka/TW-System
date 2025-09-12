0fb97bb9d7bdb2a2cd2aa4887767a051aaf464dc
Lucas <P0659387@portoseguro.com.br>
2025-09-12 12:48:26 -0300

    Update environment API URLs for development and production; add .netlify to .gitignore and include _redirects in angular.json

--- src/environments/environment.hml.ts
+++ src/environments/environment.hml.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://localhost:3000/api/v1',
+    apiUrl: 'https://tw-system-api.onrender.com/api/v1',
     appName: 'TW-System Dev'
 };
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    apiUrl: 'http://localhost:3000/api/v1',
+    apiUrl: 'https://tw-system-api.onrender.com/api/v1',
     appName: 'TW-System Dev'
 };
